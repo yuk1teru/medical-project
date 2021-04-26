ea26112cb622ddd731ebca05e352c2ec4f7c929c
yuk1teru <artemk0b2396@gmail.com>
2021-04-26 21:46:14 +0300

    Футер сайта

--- app/js/main.js
+++ app/js/main.js
@@ -22,7 +22,17 @@
        
         langList.classList.toggle("visibility");
       
-    });
+     });
     
+
    
 })();
+
+
+function slowScroll (id) {
+    var offset = 70;
+    $('html, body').animate({
+        scrollTop: $(id).offset ().top - offset
+    }, 500);
+    return false;
+}
