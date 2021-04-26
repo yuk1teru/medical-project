dcfdccc15fcd19286b756057fb42c16968e4ad30
yuk1teru <artemk0b2396@gmail.com>
2021-04-26 17:37:34 +0300

    Шапка сайта

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,28 @@
-$(function () {
+(() => {
+    const servicesBtn = document.querySelector("[services]");
+    const servicesList = document.querySelector("[services-list]");
+    const aboutBtn = document.querySelector("[about]");
+    const aboutList = document.querySelector("[about-list]");
+    const langBtn = document.querySelector("[langBtn]");
+    const langList = document.querySelector("[lang-list]");
 
-})
+    servicesBtn.addEventListener("click", () => {
+       
+        servicesList.classList.toggle("visibility");
+      
+    });
+
+     aboutBtn.addEventListener("click", () => {
+       
+        aboutList.classList.toggle("visibility");
+      
+     });
+    
+     langBtn.addEventListener("click", () => {
+       
+        langList.classList.toggle("visibility");
+      
+    });
+    
+   
+})();
